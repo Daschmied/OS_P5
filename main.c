31e1a3e8379be77f224463f803ccde4370c50675
Andrew D Paek <apaek1@student00.cse.nd.edu>
2017-04-07 15:53:52 -0400

    do custom

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
-
+#include <ctype.h>
 int changeFrame = 0;
 int page_faults = 0;
 int disk_reads = 0;
@@ -98,27 +98,94 @@ void page_fault_handler( struct page_table *pt, int page )
     disk_reads++;
     disk_writes++;
   }
-  /*
-     else if(!strcmp(method, "custom")) {
-     page_table_set_entry(pt, page, page, PROT_READ|PROT_WRITE);
-     printf("page fault on page #%d\n",page);
 
-     }
-     else {
-     printf("Page_handler not found\n");
-     exit(1);
-     }
-     */
+  else if(!strcmp(method, "custom") && full == 1) {
+    //find dirty pages
+    int i = 0;
+    int frame_num = page_table_get_nframes(pt);
+    int kickpage; 
+    int ntframe;
+    int ntbits;
+    int dirty_found = 0;
+    for(i = 0; i < frame_num; i++) {
+      //search for any dirty bits
+      page_table_get_entry(pt, frame_table[i], &ntframe, &ntbits);
+      if(ntbits == 3){
+        kickpage = frame_table[i];
+        dirty_found = 1;
+        break;
+      }
+
+    }
+    if(dirty_found == 1){
+      //use dirty bits
+      disk_write(disk, kickpage, &physmem[ntframe*PAGE_SIZE]);
+      disk_read(disk, page, &physmem[ntframe*PAGE_SIZE]);
+      page_table_set_entry(pt, page, ntframe, PROT_READ);  
+      page_table_set_entry(pt, kickpage, 0, 0); 
+      disk_reads++;
+      disk_writes++;
+    }
+    else{
+      //use rand if no dirty bits
+      srand(time(NULL));
+      //srand(page_table_get_npages(pt)); 
+      int replace = rand()%page_table_get_npages(pt);
+      int rpframe;
+      int rpbits;
+      page_table_get_entry(pt, replace, &rpframe, &rpbits);
+      while(rpbits == 0){
+        replace = rand() % page_table_get_npages(pt);
+        page_table_get_entry(pt, replace, &rpframe, &rpbits);
+      }
+      int npframe;
+      int npbits;
+      page_table_get_entry(pt, page, &npframe, &npbits);
+      disk_write(disk, replace, &physmem[rpframe*PAGE_SIZE]);
+      disk_read(disk, page, &physmem[rpframe*PAGE_SIZE]);
+      disk_reads++;
+      disk_writes++;
+      page_table_set_entry(pt, page, rpframe, PROT_READ);  
+      page_table_set_entry(pt, replace, 0, 0);
+      
+      
+    }
+
+    //page_table_set_entry(pt, page, page, PROT_READ|PROT_WRITE);
+    //printf("page fault on page #%d\n",page);
+
+  }
+
+  else if(full == 1) {
+    printf("Page_handler not found\n");
+    exit(1);
+  }
 
 }
-
+int isNumber(char number[]){
+  int i=0;
+  for (; number[i]!=0; i++){
+    if(!isdigit(number[i]))
+      return 0;
+  }
+  return 1;
+}
 int main( int argc, char *argv[] )
 {
   if(argc!=5) {
     printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
     return 1;
   }
+  if(!isNumber(argv[1])){
+    printf("Error: npages must be a positive integer\n");
+    return 1;
 
+  }
+  if(!isNumber(argv[2])){
+    printf("Error: nframes must be a positive integer\n");
+    return 1;
+
+  }
   int npages = atoi(argv[1]);
   int nframes = atoi(argv[2]);
   method = argv[3];
@@ -126,12 +193,16 @@ int main( int argc, char *argv[] )
 
   //frame_table stuff
   frame_table = malloc(sizeof(int)*nframes);
-
-  int i;
-  for(i = 0; i < nframes; i++) {
-    frame_table[i] = -1;
+  if(frame_table == NULL){
+    fprintf(stderr, "unable to malloc: %s\n", strerror(errno));
+    return 1;
   }
-
+  else{
+    int i;
+    for(i = 0; i < nframes; i++) {
+      frame_table[i] = -1;
+    }
+  }
 
   disk = disk_open("myvirtualdisk",npages);
   if(!disk) {
